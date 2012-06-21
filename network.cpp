584a3646c304f4d8d5c15911b6ab431b13d97ae0
Konstantin Kolodiy <bone@land.ru>
2012-06-21 10:23:35 +0900

    Improved source code Bugfixing Added target Harmattan

--- network.cpp
+++ network.cpp
@@ -1,5 +1,6 @@
 #include "network.h"
 #include <QDateTime>
+#include <QDebug>
 
 void Network::run()
 {
@@ -8,6 +9,7 @@ void Network::run()
 
 void Network::send(const Pack_t &packet)
 {
+    qDebug() << packet.toJson();
     QTcpSocket soc;
     soc.connectToHost("192.168.0.66",3456, QTcpSocket::WriteOnly);
     soc.waitForConnected();
@@ -19,12 +21,12 @@ void Network::send(const Pack_t &packet)
 
 QString Pack_t::toJson(void) const
 {
-    QString json("[\"%0\", \"%1\", %2, %3,
+    QString json("[\"%0\", \"%1\", %2, %3,"
                  "%4, %5, %6, %7, \"%8\","
                  "\"%9\", \"%10\", \"%11\","
                  "\"%12\", \"%13\", \"%14\"]");
 
-    return json.arg(imei).arg(name).arg(gsm).arg(QDateTime::currentDateTime().toTime_t()())
+    return json.arg(imei).arg(name).arg(gsm).arg(QDateTime::currentDateTime().toTime_t())
             .arg(cdma).arg(wcdma).arg(altitude).arg(longitude).arg(azimuth)
             .arg(xMag).arg(yMag).arg(zMag)
             .arg(xAc).arg(yAc).arg(zAc);
